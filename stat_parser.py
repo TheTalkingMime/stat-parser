033d8d1f93cb4d2b1ccd68e382c838dbabe626a7
TheTalkingMime <talkingmime1@gmail.com>
2024-04-09 20:47:24 -0400

    Refactored everything

--- stat_parser.py
+++ stat_parser.py
@@ -2,71 +2,90 @@ import os
 import json
 import csv
 import requests
+from pathlib import Path
 from collections import defaultdict
 
-json_files = [pos_json for pos_json in os.listdir('stats_jsons') if pos_json.endswith('.json')]
-data_list = {}
-
-for json_file in json_files:
-    full_path = os.path.join('stats_jsons/', json_file)
-    with open(full_path, 'r') as f:
-        data = json.load(f)
-        data_list[json_file[:-5]] = data
+CWD = Path(__file__).resolve().parent
 
 # https://minecraft.wiki/w/Statistics
+def main():
+    all_player_stats = defaultdict(list)
+    raw_stats = load_stat_files()
+    data_format = load_data_format()
 
-bac_columns = ['UUID', 'Name', 'Time', 'Distance By Elytra', 'Fireworks Used', 'Deaths', 'Totems Used', 'Shulkers Opened', 'Diamond Pickaxe Uses', 'Diamond Shovel Uses', 'Diamond Axe Uses', 'Diamond Hoe Uses']
-bac_stats = [
-    ["stats", "minecraft:custom", "minecraft:play_time"],
-    ["stats", "minecraft:custom", "minecraft:aviate_one_cm"],
-    ["stats", "minecraft:used", "minecraft:firework_rocket"],
-    ["stats", "minecraft:custom", "minecraft:deaths"],
-    ["stats", "minecraft:used", "minecraft:totem_of_undying"],
-    ["stats", "minecraft:custom", "minecraft:open_shulker_box"],
-    ["stats", "minecraft:used", "minecraft:diamond_pickaxe"],
-    ["stats", "minecraft:used", "minecraft:diamond_shovel"],
-    ["stats", "minecraft:used", "minecraft:diamond_axe"],
-    ["stats", "minecraft:used", "minecraft:diamond_hoe"],
-]
+    for uuid in raw_stats:
+        ign = get_ign(uuid)
+        player_raw_stats = raw_stats[uuid]
 
-melon_columns = ['UUID', 'Name', 'Melons', 'Time', 'Dirt Mined', 'Grass Mined', 'Dirt Used', 'Grass Used', 'Moss Used', 'Distance By Elytra', 'Fireworks Used', 'Melon Seeds Used']
-melon_stats = [
-    ["stats", "minecraft:picked_up", "minecraft:melon_slice"],
-    ["stats", "minecraft:custom", "minecraft:play_time"],
-    ["stats", "minecraft:mined", "minecraft:dirt"],
-    ["stats", "minecraft:mined", "minecraft:grass_block"],
-    ["stats", "minecraft:used", "minecraft:dirt"],
-    ["stats", "minecraft:used", "minecraft:grass_block"],
-    ["stats", "minecraft:used", "minecraft:moss_block"],
-    ["stats", "minecraft:custom", "minecraft:aviate_one_cm"],
-    ["stats", "minecraft:used", "minecraft:firework_rocket"],
-    ["stats", "minecraft:used", "minecraft:melon_seeds"],
-]
+        for root in data_format:
+            if root == "stats":
+                all_player_stats[ign] = get_stats(data_format[root], player_raw_stats[root])
+    write_data(get_columns(data_format), all_player_stats)
 
-columns = bac_columns
-interesting_stats = bac_stats
 
-indiv_counts = defaultdict(list)
-count = 0
-for uuid in data_list:
-    stats_file = data_list[uuid]
+def get_stats(data_format, stats_file):
+    stats = []
+    for stat_type in data_format:
+        if stat_type not in stats_file:
+            stats.append(0)
+            continue
+        for objective in data_format[stat_type]:
+            if objective not in stats_file[stat_type]:
+                stats.append(0)
+                continue
+            stats.append(stats_file[stat_type][objective])
+    return stats
 
+
+def get_columns(data_format):
+    columns = ['ign']
+    for root in data_format:
+        for data_type in data_format[root]:
+            for objective in data_format[root][data_type]:
+                columns.append(data_format[root][data_type][objective])
+    return columns
+
+
+def stat_exists(stats_file, stat):
+    try:
+        return stats_file[stat[0]][stat[1]][stat[2]]
+    except KeyError:
+        return ""
+
+
+def get_ign(uuid):
     # Mojang API Request - UUID to IGN
-    r = requests.get('https://sessionserver.mojang.com/session/minecraft/profile/' + uuid)
-    indiv_counts[uuid].append(r.json()['name'])
+    r = requests.get(f'https://sessionserver.mojang.com/session/minecraft/profile/{uuid}')
+    return r.json()['name']
 
-    for stat in interesting_stats:
-        if stat[0] in stats_file and stat[1] in stats_file[stat[0]] and stat[2] in stats_file[stat[0]][stat[1]]:
-            stat_count = stats_file[stat[0]][stat[1]][stat[2]]
-            indiv_counts[uuid].append(stat_count)
-        else:
-            indiv_counts[uuid].append(0)
 
-with open('stats.csv', mode='w', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerow(columns)
-    for key, values in indiv_counts.items():
-        writer.writerow([key] + values)
+def load_data_format(filename='format.json'):
+    filename = os.path.join(CWD, filename)
+    with open(filename, 'r') as f:
+        data_format = json.load(f)
+    return data_format
 
-print(count)
-print(indiv_counts)
+
+def load_stat_files(filename='stats_jsons'):
+    stats_folder = os.path.join(CWD, filename)
+    json_files = [pos_json for pos_json in os.listdir(stats_folder) if pos_json.endswith('.json')]
+    data_list = {}
+
+    for json_file in json_files:
+        full_path = os.path.join(stats_folder, json_file)
+        with open(full_path, 'r') as f:
+            data = json.load(f)
+            data_list[json_file[:-5]] = data
+    return data_list
+
+
+def write_data(column_headers, rows):
+    filename = os.path.join(CWD, 'stats.csv')
+    with open(filename, mode='w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(column_headers)
+        for key, values in rows.items():
+            writer.writerow([key] + values)
+
+
+main()
